f8e895a77f57f042e7d6892b782cf4f7cb9d5bc1
Robert James Patterson <guajiropa@hotmail.com>
2018-02-18 19:03:26 -0500

    Update DBFirstTestScript.sql

--- DBFirstTestScript.sql
+++ DBFirstTestScript.sql
@@ -1,4 +1,4 @@
-﻿/*
+/*
 	Run this T-SQL script in Microsoft SQL Server Management Studio to create the database
 	used in the early videos in the Treehouse Entity Framework Basics Model First video. 
 */
@@ -7,12 +7,12 @@ GO
 CREATE DATABASE DatabaseFirstTestDB ON ( NAME = DBFirstData, 
 					FILENAME = 'D:\MSSQL Server Data\DBFirstTest.mdf',
 					SIZE = 10MB,
-					MAXSIZE = 500MB,
+					MAXSIZE = 50MB,
 					FILEGROWTH = 5MB )
 					LOG ON ( NAME = DBFirstLog,
 					FILENAME = 'D:\MSSQL Server Data\DBFirstLog.ldf',
 					SIZE = 5MB,
-					MAXSIZE = 250MB,
+					MAXSIZE = 25MB,
 					FILEGROWTH = 5MB);
 GO
 USE [DatabaseFirstTestDB];
@@ -22,15 +22,15 @@ GO
 SET QUOTED_IDENTIFIER ON
 GO
 CREATE TABLE [dbo].[Contact](
-	[Id] [uniqueidentifier] NOT NULL,
+	[Id] [int] NOT NULL PRIMARY KEY,
 	[Name] [nchar](30) NULL,
 	[Nickname] [nchar](30) NULL,
 	[Age] [numeric](18, 0) NULL
 ) ON [PRIMARY]
 GO
 CREATE TABLE [dbo].[ContactAddress](
-	[Id] [uniqueidentifier] NOT NULL,
-	[ContactId] [nvarchar](50) NOT NULL,
+	[Id] [int] NOT NULL PRIMARY KEY,
+	[ContactId] [int] FOREIGN KEY References Contact(Id),
 	[AddressLine1] [nchar](20) NULL,
 	[AddressLine2] [nchar](20) NULL,
 	[City] [nchar](20) NULL,
@@ -39,8 +39,8 @@ CREATE TABLE [dbo].[ContactAddress](
 ) ON [PRIMARY]
 GO
 CREATE TABLE [dbo].[ContactEmail](
-	[Id] [uniqueidentifier] NOT NULL,
-	[ContactId] [nvarchar](50) NOT NULL,
+	[Id] [int] NOT NULL PRIMARY KEY,
+	[ContactId] [int] FOREIGN KEY References Contact(Id),
 	[Email] [nvarchar](100) NULL
 ) ON [PRIMARY]
 GO
